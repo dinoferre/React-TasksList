a53bb47cbf29f573c50e173caf0dbd1ab87d3aa7
Dino Ferre <dinoferre@hotmail.com>
2022-12-21 15:30:56 -0300

    Upload

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: 'Mi Primer tarea',
+    description: "Mi Primer tarea"
+  },
+  {
+    id: 1,
+    title: 'Mi Segunda tarea',
+    description: "Mi Segunda tarea"
+  },
+  {
+    id: 2,
+    title: 'Mi Tercer tarea',
+    description: "Mi Tercer tarea"
+  }
+]
